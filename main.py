f4f935ebb71f4c5b3567b6cdd5e2137924cdc35a
Arseniy <yarmolinskiyam@gmail.com>
2023-12-04 10:52:21 +0000

    Пример кода на py

---
+++ main.py
@@ -0,0 +1,34 @@
+x = 10
+xf = 10.0
+
+a, b = 20, 30
+
+bool_varT = True
+bool_varF = False
+bool_logic = a < b 
+
+string = "ЭТО СТРОКА!!!"
+string2 = 'Это тоже строка)'
+
+string_with_quotes = 'Строка с "кавычками"'
+string_with_quotes2 = "Строка с 'кавычками'"
+string_with_quotes2 = "Строка с \"кавычками\""
+
+Pr = "Пр"
+ivet = "ивет"
+Privet = Pr + " " + ivet # Пр ивет
+
+xstr = str(x) # xstr == "10"
+
+int(0.99) # 0
+int(-0.99) # -1
+float(123) # 123.0
+
+a, b = 20, 6
+a + b # 26
+a - b # 14
+a*b # 120
+a/b # 3.3333333333333334
+a//b # 3
+a%b # 2
+a**b # 20**6
